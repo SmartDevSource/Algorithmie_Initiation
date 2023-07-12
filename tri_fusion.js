873dc0fe4044e006a92f0a7ce532ea5ca7d1b175
SmartDevSource <dreamrock@hotmail.fr>
2023-07-12 11:35:36 +0200

    .

--- tri_fusion.js
+++ tri_fusion.js
@@ -1,16 +1,27 @@
 const fs = require("fs");
 
-let dataFile, list;
+let dataFile, lists;
 
 try{
   dataFile = fs.readFileSync(process.argv[2], "utf-8");
-  list = JSON.parse(dataFile).list;
+  lists = JSON.parse(dataFile);
 }catch{
   console.log("fichier inexistant");
 }
 
-console.log("Etat d'origine de la liste : "+list);
+console.log("Etat d'origine des listes : "+lists.firstList + " et " + lists.lastList);
 
 function mergeSort(list){
-  
+  if (list.length <2) return list;
+  return merge(mergeSort(list.splice(0, (list.length/2))), mergeSort(list));
 }
+
+function merge(l, r){
+  let a = new Array();
+  while(l.length && r.length) if (l[0] < r[0]) { counter++; a.push(l.shift()); } else { counter++; a.push(r.shift())};
+  return [...a, ...l,...r];
+}
+
+let counter = 0;
+console.log(mergeSort(lists.firstList));
+console.log("Nombre de comparaisons effectuées : "+counter);
